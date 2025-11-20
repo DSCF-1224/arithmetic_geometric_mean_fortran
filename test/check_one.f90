53884b7e6e0a1c70cf4602800d987106446f18bc
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-11-21 07:18:05 +0900

    test: add check_one test for AGM(1,1) case
    Add test to verify that arithmetic_geometric_mean(1, 1) returns exactly 1 for real32, real64, and real128 types.
    Include both fypp template and generated Fortran source, and update CI workflow to preprocess the new test.
    
    git merge develop_test --squash

---
+++ test/check_one.f90
@@ -0,0 +1,73 @@
+program check_one
+
+    use, intrinsic :: iso_fortran_env, only: real32
+    use, intrinsic :: iso_fortran_env, only: real64
+    use, intrinsic :: iso_fortran_env, only: real128
+
+    use, non_intrinsic :: arithmetic_geometric_mean_fortran
+
+    use, non_intrinsic :: ieee_class_fortran
+
+
+
+    implicit none
+
+
+
+    call test_real32
+    call test_real64
+    call test_real128
+
+
+
+    contains
+
+
+
+    subroutine test_real32
+
+        real(real32), parameter :: one = 1.0_real32
+
+        real(real32) :: agm
+
+
+
+        agm = arithmetic_geometric_mean(one, one)
+
+        if ( .not. is_ieee_positive_zero(agm - one) ) error stop
+
+    end subroutine test_real32
+
+
+
+    subroutine test_real64
+
+        real(real64), parameter :: one = 1.0_real64
+
+        real(real64) :: agm
+
+
+
+        agm = arithmetic_geometric_mean(one, one)
+
+        if ( .not. is_ieee_positive_zero(agm - one) ) error stop
+
+    end subroutine test_real64
+
+
+
+    subroutine test_real128
+
+        real(real128), parameter :: one = 1.0_real128
+
+        real(real128) :: agm
+
+
+
+        agm = arithmetic_geometric_mean(one, one)
+
+        if ( .not. is_ieee_positive_zero(agm - one) ) error stop
+
+    end subroutine test_real128
+
+end program check_one
