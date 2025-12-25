e268428010c15f9222a7c0fb515f1721180d75ff
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-12-26 06:44:36 +0900

    feat: add object-oriented AGM computation with history tracking
    Introduce type-based AGM implementation with iteration history:
    - Add abstract base type and real kind-specific derived types
    - Implement public compute() method with input validation
    - Add private compute_kernel() for core iteration algorithm
    - Track complete iteration history in list_a and list_g arrays
    - Extract helper functions: compute_step, is_not_converged, initialize
    - Refactor existing functions to use new helper routines
    - Add sentinel values and precision-dependent iteration limits

--- src/arithmetic_geometric_mean_fortran.f90
+++ src/arithmetic_geometric_mean_fortran.f90
@@ -20,6 +20,19 @@ module arithmetic_geometric_mean_fortran
     public :: arithmetic_geometric_mean
     public :: arithmetic_geometric_mean_kernel
 
+    public :: arithmetic_geometric_mean_real32_type
+    public :: arithmetic_geometric_mean_real64_type
+    public :: arithmetic_geometric_mean_real128_type
+
+
+
+    integer, parameter :: initial_n_iter = -1
+
+    integer, parameter :: max_n_iter_real32 = digits(0.0_real32)
+    integer, parameter :: max_n_iter_real64 = digits(0.0_real64)
+    integer, parameter :: max_n_iter_real128 = digits(0.0_real128)
+
+
 
 
     interface arithmetic_geometric_mean
@@ -64,6 +77,99 @@ module arithmetic_geometric_mean_fortran
         module procedure :: arithmetic_geometric_mean_kernel_real128
 
     end interface arithmetic_geometric_mean_kernel
+
+
+
+    interface compute_step
+        module procedure :: compute_step_real32
+        module procedure :: compute_step_real64
+        module procedure :: compute_step_real128
+    end interface compute_step
+
+
+
+    interface initialize
+        module procedure :: initialize_real32
+        module procedure :: initialize_real64
+        module procedure :: initialize_real128
+    end interface initialize
+
+
+
+    interface is_not_converged
+        module procedure :: is_not_converged_real32
+        module procedure :: is_not_converged_real64
+        module procedure :: is_not_converged_real128
+    end interface is_not_converged
+
+
+
+    type, abstract :: arithmetic_geometric_mean_base_type
+
+        integer, private :: n_iter = initial_n_iter
+        !! the number of iterations performed during AGM calculation
+
+    end type arithmetic_geometric_mean_base_type
+
+
+
+    type, extends(arithmetic_geometric_mean_base_type) :: arithmetic_geometric_mean_real32_type
+
+        real(real32), private :: list_a(0:max_n_iter_real32)
+        !! history of the arithmetic mean
+
+        real(real32), private :: list_g(0:max_n_iter_real32)
+        !! history of the geometric mean
+
+        contains
+
+        procedure, pass, private :: compute_real32
+        procedure, pass, private :: compute_kernel_real32
+
+        generic, public  :: compute        => compute_real32
+        generic, private :: compute_kernel => compute_kernel_real32
+
+    end type arithmetic_geometric_mean_real32_type
+
+
+
+    type, extends(arithmetic_geometric_mean_base_type) :: arithmetic_geometric_mean_real64_type
+
+        real(real64), private :: list_a(0:max_n_iter_real64)
+        !! history of the arithmetic mean
+
+        real(real64), private :: list_g(0:max_n_iter_real64)
+        !! history of the geometric mean
+
+        contains
+
+        procedure, pass, private :: compute_real64
+        procedure, pass, private :: compute_kernel_real64
+
+        generic, public  :: compute        => compute_real64
+        generic, private :: compute_kernel => compute_kernel_real64
+
+    end type arithmetic_geometric_mean_real64_type
+
+
+
+    type, extends(arithmetic_geometric_mean_base_type) :: arithmetic_geometric_mean_real128_type
+
+        real(real128), private :: list_a(0:max_n_iter_real128)
+        !! history of the arithmetic mean
+
+        real(real128), private :: list_g(0:max_n_iter_real128)
+        !! history of the geometric mean
+
+        contains
+
+        procedure, pass, private :: compute_real128
+        procedure, pass, private :: compute_kernel_real128
+
+        generic, public  :: compute        => compute_real128
+        generic, private :: compute_kernel => compute_kernel_real128
+
+    end type arithmetic_geometric_mean_real128_type
 
 
 
@@ -272,9 +378,9 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        real(real32) :: last_a !! last arithmetic mean
+        real(real32) :: prev_a !! previous arithmetic mean
 
-        real(real32) :: last_g !! last geometric mean
+        real(real32) :: prev_g !! previous geometric mean
 
         real(real32) :: next_a !! next arithmetic mean
 
@@ -282,20 +388,24 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        last_a = a
-        last_g = g
+        prev_a = a
+        prev_g = g
 
 
 
         do
 
-            next_a =     (last_a + last_g) * 0.5_real32
-            next_g = sqrt(last_a * last_g)
+            call compute_step( &!
+                prev_a = prev_a , &!
+                prev_g = prev_g , &!
+                next_a = next_a , &!
+                next_g = next_g   &!
+            )
 
-            if ( abs(next_a - next_g) .gt. spacing( min(next_a, next_g) ) ) then
+            if ( is_not_converged(next_a, next_g) ) then
 
-                last_a = next_a
-                last_g = next_g
+                prev_a = next_a
+                prev_g = next_g
 
                 cycle
 
@@ -340,9 +450,9 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        real(real64) :: last_a !! last arithmetic mean
+        real(real64) :: prev_a !! previous arithmetic mean
 
-        real(real64) :: last_g !! last geometric mean
+        real(real64) :: prev_g !! previous geometric mean
 
         real(real64) :: next_a !! next arithmetic mean
 
@@ -350,20 +460,24 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        last_a = a
-        last_g = g
+        prev_a = a
+        prev_g = g
 
 
 
         do
 
-            next_a =     (last_a + last_g) * 0.5_real64
-            next_g = sqrt(last_a * last_g)
+            call compute_step( &!
+                prev_a = prev_a , &!
+                prev_g = prev_g , &!
+                next_a = next_a , &!
+                next_g = next_g   &!
+            )
 
-            if ( abs(next_a - next_g) .gt. spacing( min(next_a, next_g) ) ) then
+            if ( is_not_converged(next_a, next_g) ) then
 
-                last_a = next_a
-                last_g = next_g
+                prev_a = next_a
+                prev_g = next_g
 
                 cycle
 
@@ -408,9 +522,9 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        real(real128) :: last_a !! last arithmetic mean
+        real(real128) :: prev_a !! previous arithmetic mean
 
-        real(real128) :: last_g !! last geometric mean
+        real(real128) :: prev_g !! previous geometric mean
 
         real(real128) :: next_a !! next arithmetic mean
 
@@ -418,20 +532,24 @@ module arithmetic_geometric_mean_fortran
 
 
 
-        last_a = a
-        last_g = g
+        prev_a = a
+        prev_g = g
 
 
 
         do
 
-            next_a =     (last_a + last_g) * 0.5_real128
-            next_g = sqrt(last_a * last_g)
+            call compute_step( &!
+                prev_a = prev_a , &!
+                prev_g = prev_g , &!
+                next_a = next_a , &!
+                next_g = next_g   &!
+            )
 
-            if ( abs(next_a - next_g) .gt. spacing( min(next_a, next_g) ) ) then
+            if ( is_not_converged(next_a, next_g) ) then
 
-                last_a = next_a
-                last_g = next_g
+                prev_a = next_a
+                prev_g = next_g
 
                 cycle
 
@@ -446,5 +564,536 @@ module arithmetic_geometric_mean_fortran
         end do
 
     end function arithmetic_geometric_mean_kernel_real128
+
+
+
+    elemental function is_not_converged_real32(a, g) result(stat)
+
+        real(real32), intent(in) :: a !! arithmetic mean
+
+        real(real32), intent(in) :: g !! geometric mean
+
+
+
+        logical :: stat
+
+
+
+        stat = abs(a - g) .gt. spacing( min(a, g) )
+
+    end function is_not_converged_real32
+
+
+
+    elemental function is_not_converged_real64(a, g) result(stat)
+
+        real(real64), intent(in) :: a !! arithmetic mean
+
+        real(real64), intent(in) :: g !! geometric mean
+
+
+
+        logical :: stat
+
+
+
+        stat = abs(a - g) .gt. spacing( min(a, g) )
+
+    end function is_not_converged_real64
+
+
+
+    elemental function is_not_converged_real128(a, g) result(stat)
+
+        real(real128), intent(in) :: a !! arithmetic mean
+
+        real(real128), intent(in) :: g !! geometric mean
+
+
+
+        logical :: stat
+
+
+
+        stat = abs(a - g) .gt. spacing( min(a, g) )
+
+    end function is_not_converged_real128
+
+
+
+    elemental subroutine compute_real32(agm, x, y)
+
+        class(arithmetic_geometric_mean_real32_type), intent(inout) :: agm
+
+        real(real32), intent(in) :: x, y
+
+
+
+        real(real32) :: xy
+
+
+
+        if ( ieee_unordered(x, y) ) then
+
+            call initialize(agm); return
+
+        end if
+
+
+
+        xy = x * y
+
+
+
+        if ( xy .lt. 0.0_real32 ) then
+
+            call initialize(agm)
+
+        else if ( xy .gt. 0.0_real32 ) then
+
+            if (x .lt. y) then
+                call agm%compute_kernel(init_a = y, init_g = x)
+            else
+                call agm%compute_kernel(init_a = x, init_g = y)
+            end if
+
+        else
+
+            call initialize(agm)
+
+            if (x .lt. y) then
+
+                agm%list_a(agm%n_iter) = y
+                agm%list_g(agm%n_iter) = x
+
+            else
+
+                agm%list_a(agm%n_iter) = x
+                agm%list_g(agm%n_iter) = y
+        
+            end if
+
+            agm%n_iter = agm%n_iter + 1
+
+            agm%list_a(agm%n_iter) = 0.0_real32
+            agm%list_g(agm%n_iter) = 0.0_real32
+
+        end if
+
+    end subroutine compute_real32
+
+
+
+    elemental subroutine compute_real64(agm, x, y)
+
+        class(arithmetic_geometric_mean_real64_type), intent(inout) :: agm
+
+        real(real64), intent(in) :: x, y
+
+
+
+        real(real64) :: xy
+
+
+
+        if ( ieee_unordered(x, y) ) then
+
+            call initialize(agm); return
+
+        end if
+
+
+
+        xy = x * y
+
+
+
+        if ( xy .lt. 0.0_real64 ) then
+
+            call initialize(agm)
+
+        else if ( xy .gt. 0.0_real64 ) then
+
+            if (x .lt. y) then
+                call agm%compute_kernel(init_a = y, init_g = x)
+            else
+                call agm%compute_kernel(init_a = x, init_g = y)
+            end if
+
+        else
+
+            call initialize(agm)
+
+            if (x .lt. y) then
+
+                agm%list_a(agm%n_iter) = y
+                agm%list_g(agm%n_iter) = x
+
+            else
+
+                agm%list_a(agm%n_iter) = x
+                agm%list_g(agm%n_iter) = y
+        
+            end if
+
+            agm%n_iter = agm%n_iter + 1
+
+            agm%list_a(agm%n_iter) = 0.0_real64
+            agm%list_g(agm%n_iter) = 0.0_real64
+
+        end if
+
+    end subroutine compute_real64
+
+
+
+    elemental subroutine compute_real128(agm, x, y)
+
+        class(arithmetic_geometric_mean_real128_type), intent(inout) :: agm
+
+        real(real128), intent(in) :: x, y
+
+
+
+        real(real128) :: xy
+
+
+
+        if ( ieee_unordered(x, y) ) then
+
+            call initialize(agm); return
+
+        end if
+
+
+
+        xy = x * y
+
+
+
+        if ( xy .lt. 0.0_real128 ) then
+
+            call initialize(agm)
+
+        else if ( xy .gt. 0.0_real128 ) then
+
+            if (x .lt. y) then
+                call agm%compute_kernel(init_a = y, init_g = x)
+            else
+                call agm%compute_kernel(init_a = x, init_g = y)
+            end if
+
+        else
+
+            call initialize(agm)
+
+            if (x .lt. y) then
+
+                agm%list_a(agm%n_iter) = y
+                agm%list_g(agm%n_iter) = x
+
+            else
+
+                agm%list_a(agm%n_iter) = x
+                agm%list_g(agm%n_iter) = y
+        
+            end if
+
+            agm%n_iter = agm%n_iter + 1
+
+            agm%list_a(agm%n_iter) = 0.0_real128
+            agm%list_g(agm%n_iter) = 0.0_real128
+
+        end if
+
+    end subroutine compute_real128
+
+
+
+    elemental subroutine compute_kernel_real32(agm, init_a, init_g)
+
+        class(arithmetic_geometric_mean_real32_type), intent(inout) :: agm
+
+        real(real32), intent(in) :: init_a
+        !! initial value: arithmetic mean
+
+        real(real32), intent(in) :: init_g
+        !! initial value: geometric mean
+
+
+
+        call initialize(agm)
+
+        agm%list_a(0) = init_a
+        agm%list_g(0) = init_g
+        agm%n_iter    = 0
+
+        do
+
+            associate(prev_iter => agm%n_iter, next_iter => agm%n_iter + 1)
+
+                agm%n_iter = next_iter
+
+                associate( &!
+                    prev_a => agm%list_a(prev_iter) , &!
+                    prev_g => agm%list_g(prev_iter) , &!
+                    next_a => agm%list_a(next_iter) , &!
+                    next_g => agm%list_g(next_iter)   &!
+                )
+
+                    call compute_step( &!
+                        prev_a = prev_a , &!
+                        prev_g = prev_g , &!
+                        next_a = next_a , &!
+                        next_g = next_g   &!
+                    )
+
+                    if ( is_not_converged(next_a, next_g) ) then
+
+                        cycle
+
+                    else
+
+                        return
+
+                    end if
+
+                end associate
+
+            end associate
+
+        end do
+
+    end subroutine compute_kernel_real32
+
+
+
+    elemental subroutine compute_kernel_real64(agm, init_a, init_g)
+
+        class(arithmetic_geometric_mean_real64_type), intent(inout) :: agm
+
+        real(real64), intent(in) :: init_a
+        !! initial value: arithmetic mean
+
+        real(real64), intent(in) :: init_g
+        !! initial value: geometric mean
+
+
+
+        call initialize(agm)
+
+        agm%list_a(0) = init_a
+        agm%list_g(0) = init_g
+        agm%n_iter    = 0
+
+        do
+
+            associate(prev_iter => agm%n_iter, next_iter => agm%n_iter + 1)
+
+                agm%n_iter = next_iter
+
+                associate( &!
+                    prev_a => agm%list_a(prev_iter) , &!
+                    prev_g => agm%list_g(prev_iter) , &!
+                    next_a => agm%list_a(next_iter) , &!
+                    next_g => agm%list_g(next_iter)   &!
+                )
+
+                    call compute_step( &!
+                        prev_a = prev_a , &!
+                        prev_g = prev_g , &!
+                        next_a = next_a , &!
+                        next_g = next_g   &!
+                    )
+
+                    if ( is_not_converged(next_a, next_g) ) then
+
+                        cycle
+
+                    else
+
+                        return
+
+                    end if
+
+                end associate
+
+            end associate
+
+        end do
+
+    end subroutine compute_kernel_real64
+
+
+
+    elemental subroutine compute_kernel_real128(agm, init_a, init_g)
+
+        class(arithmetic_geometric_mean_real128_type), intent(inout) :: agm
+
+        real(real128), intent(in) :: init_a
+        !! initial value: arithmetic mean
+
+        real(real128), intent(in) :: init_g
+        !! initial value: geometric mean
+
+
+
+        call initialize(agm)
+
+        agm%list_a(0) = init_a
+        agm%list_g(0) = init_g
+        agm%n_iter    = 0
+
+        do
+
+            associate(prev_iter => agm%n_iter, next_iter => agm%n_iter + 1)
+
+                agm%n_iter = next_iter
+
+                associate( &!
+                    prev_a => agm%list_a(prev_iter) , &!
+                    prev_g => agm%list_g(prev_iter) , &!
+                    next_a => agm%list_a(next_iter) , &!
+                    next_g => agm%list_g(next_iter)   &!
+                )
+
+                    call compute_step( &!
+                        prev_a = prev_a , &!
+                        prev_g = prev_g , &!
+                        next_a = next_a , &!
+                        next_g = next_g   &!
+                    )
+
+                    if ( is_not_converged(next_a, next_g) ) then
+
+                        cycle
+
+                    else
+
+                        return
+
+                    end if
+
+                end associate
+
+            end associate
+
+        end do
+
+    end subroutine compute_kernel_real128
+
+
+
+    elemental subroutine compute_step_real32(prev_a, prev_g, next_a, next_g)
+
+        real(real32), intent(in) :: prev_a
+        !! previous arithmetic mean
+
+        real(real32), intent(in) :: prev_g
+        !! previous geometric mean
+
+        real(real32), intent(out) :: next_a
+        !! next arithmetic mean
+
+        real(real32), intent(out) :: next_g
+        !! next geometric mean
+
+
+
+        next_a =     (prev_a + prev_g) * 0.5_real32
+        next_g = sqrt(prev_a * prev_g)
+
+    end subroutine compute_step_real32
+
+
+
+    elemental subroutine compute_step_real64(prev_a, prev_g, next_a, next_g)
+
+        real(real64), intent(in) :: prev_a
+        !! previous arithmetic mean
+
+        real(real64), intent(in) :: prev_g
+        !! previous geometric mean
+
+        real(real64), intent(out) :: next_a
+        !! next arithmetic mean
+
+        real(real64), intent(out) :: next_g
+        !! next geometric mean
+
+
+
+        next_a =     (prev_a + prev_g) * 0.5_real64
+        next_g = sqrt(prev_a * prev_g)
+
+    end subroutine compute_step_real64
+
+
+
+    elemental subroutine compute_step_real128(prev_a, prev_g, next_a, next_g)
+
+        real(real128), intent(in) :: prev_a
+        !! previous arithmetic mean
+
+        real(real128), intent(in) :: prev_g
+        !! previous geometric mean
+
+        real(real128), intent(out) :: next_a
+        !! next arithmetic mean
+
+        real(real128), intent(out) :: next_g
+        !! next geometric mean
+
+
+
+        next_a =     (prev_a + prev_g) * 0.5_real128
+        next_g = sqrt(prev_a * prev_g)
+
+    end subroutine compute_step_real128
+
+
+
+    elemental subroutine initialize_real32(agm)
+
+        type(arithmetic_geometric_mean_real32_type), intent(inout) :: agm
+
+
+
+        agm%n_iter = initial_n_iter
+
+        agm%list_a(:) = ieee_value( x = 0.0_real32, class = ieee_quiet_nan )
+        agm%list_g(:) = agm%list_a(:)
+
+    end subroutine initialize_real32
+
+
+
+    elemental subroutine initialize_real64(agm)
+
+        type(arithmetic_geometric_mean_real64_type), intent(inout) :: agm
+
+
+
+        agm%n_iter = initial_n_iter
+
+        agm%list_a(:) = ieee_value( x = 0.0_real64, class = ieee_quiet_nan )
+        agm%list_g(:) = agm%list_a(:)
+
+    end subroutine initialize_real64
+
+
+
+    elemental subroutine initialize_real128(agm)
+
+        type(arithmetic_geometric_mean_real128_type), intent(inout) :: agm
+
+
+
+        agm%n_iter = initial_n_iter
+
+        agm%list_a(:) = ieee_value( x = 0.0_real128, class = ieee_quiet_nan )
+        agm%list_g(:) = agm%list_a(:)
+
+    end subroutine initialize_real128
 
 end module arithmetic_geometric_mean_fortran
